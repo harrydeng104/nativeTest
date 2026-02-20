de24a8cdcadc0dc736f2982142b919888a6ab72f
harrydeng104 <harrydeng104@gmail.com>
2026-02-20 10:15:51 -0500

    class save

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { ScrollView, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1">
-        <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+    <View className="flex-1 text-primary justify-center items-center">
+        <Text className="text-5xl font-bold text-primary">hiya</Text>
+        {/* <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
 
-        </ScrollView>
+        </ScrollView> */}
     </View>
   );
 }
